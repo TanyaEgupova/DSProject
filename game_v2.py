eb35dfd7cbd09b7db7d74ee33fc1a0cf71239830
TanyaEgupova <tatianamirfudinova@gmail.com>
2022-11-29 14:57:36 +0400

    innitial commit

--- game_v2.py
+++ game_v2.py
@@ -1,9 +1,11 @@
-
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
 
 import numpy as np
 
 
-def random_predict(number: int = np.random.randint(1, 101)) -> int:
+def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -38,13 +40,14 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(20))  # загадали список чисел
+    print(random_array)
 
     for number in random_array:
         count_ls.append(random_predict(number))
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print("Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
 # RUN
